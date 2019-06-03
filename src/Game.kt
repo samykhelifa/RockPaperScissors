01062979433d27287f4ab31378a20b2d083d9653
samy <39716963+samykhelifa@users.noreply.github.com>
2019-06-03 23:45:03 +0200

    Game.kt   simple refactoring

--- src/Game.kt
+++ src/Game.kt
@@ -1,7 +1,7 @@
 fun main() {
    val options = arrayOf("Rock","Paper","Scissors")
    val gameChoice = getGameChoice(options)
-   println(gameChoice)
+  
 }
-fun getGameChoice(optionsParam: Array<String>):String =
+fun getGameChoice(optionsParam: Array<String>) =
     optionsParam[(Math.random() * optionsParam.size).toInt()]
